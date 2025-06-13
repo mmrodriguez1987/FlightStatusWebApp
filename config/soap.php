623551530eadc4dd94713195662dc7bfc47e9681
Marcos Michael Rodriguez <mmrodriguez1987@gmail.com>
2025-06-13 19:05:32 -0400

    delete the prior project
    Now we are changing to Springboot

--- config/soap.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'wsdl'    => env('SOAP_WSDL_URL'),
-    'options' => [
-        'trace'      => true,
-        'cache_wsdl' => WSDL_CACHE_NONE,
-        // add authentication or other SoapClient flags here if needed
-    ],
-];
